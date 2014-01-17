0dd867327881af41fadc62062af49ce15d6eaa92
MKzero <info@linux-web-development.de>
2014-01-17 12:33:45 +0100

    Added phrase generator to status messages

--- twitterstatus.py
+++ twitterstatus.py
@@ -4,11 +4,23 @@ import json
 from twitter import Twitter, OAuth, TwitterHTTPError
 from time    import sleep
 from sys     import exit
+from random  import choice
 
 try:
     from config  import *
 except Exception as e:
     print("ERROR: Could not import config. Make sure config.py exists.")
+
+try:
+    from wordlist import *
+except ImportError:
+    WORDLIST = [
+        ['The space'],
+        ['is'],
+        ['open'],
+        ['closed'],
+        ['']
+    ]
 
 try:
     twitter = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET,
@@ -20,6 +32,20 @@ except Exception as e:
 def write_status(status):
     status_file = open(STATUS_FILE, 'w+')
     status_file.write(json.dumps({'status': status}))
+
+def generate_phrase(open_status=True):
+    phrase  = choice(WORDLIST[0]) + " "
+    phrase += choice(WORDLIST[1]) + " "
+
+    if open_status:
+        phrase += choice(WORDLIST[2]) + ". "
+    else:
+        phrase += choice(WORDLIST[3]) + ". "
+
+    if choice([True, False]):
+        phrase += choice(WORDLIST[4]).title() + "!"
+
+    return phrase
 
 def update(status):
     try:
@@ -37,9 +63,9 @@ def update(status):
         return
 
     if status == True and last_status['status'] == False:
-        text = 'Space is open!'
+        text = generate_phrase(True)
     elif status == False and last_status['status'] == True:
-        text = 'Space is closed!'
+        text = generate_phrase(False)
     else:
         return
 
@@ -48,7 +74,6 @@ def update(status):
     except TwitterHTTPError as e:
         print('Error while updating status: ' + e)
         return
-
 
 try:
     twitter.direct_messages.new(user='mkzer0', text='Twitter Bot Startup')
