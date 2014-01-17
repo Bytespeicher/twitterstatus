d3a18af1760ad5d1cda34f380533fe2eabe335a9
MKzero <info@linux-web-development.de>
2014-01-17 12:29:20 +0100

    Added wordlist for different status messages

---
+++ wordlist.py
@@ -0,0 +1,45 @@
+#!bin/python
+
+"""
+The word list is a list with seperate lists for 
+
+  * Space
+  * is
+  * open
+  * closed
+  * fill words
+"""
+WORDLIST = [
+    [
+        'The space', 
+        'Bytespeicher'
+    ],
+    [
+        'is',
+    ],
+    [
+        'open',
+        'allowing access',
+    ],
+    [
+        'closed',
+        'not open anymore'
+    ],
+    [
+        'magnificent', 
+        'imposing', 
+        'impressive', 
+        'awe-inspiring', 
+        'grand', 
+        'splendid', 
+        'majestic', 
+        'monumental', 
+        'glorious', 
+        'sumptuous', 
+        'resplendent', 
+        'lavish', 
+        'beautiful', 
+        'delightful', 
+        'lovely',
+    ]
+]
