94841684a3c84dc99f2191943ebfe88057a5eaf1
suicider <suicider@gmx.de>
2014-04-10 10:38:34 +0200

    Update twitterstatus.py
    Changed Admin Name for Twitter direct message contact

--- twitterstatus.py
+++ twitterstatus.py
@@ -80,7 +80,7 @@ def update(status):
         return
 
 try:
-    twitter.direct_messages.new(user='mkzer0', text='Twitter Bot Startup')
+    twitter.direct_messages.new(user=ADMIN_NAME', text='Twitter Bot Startup')
 except Exception as e:
     print('Error sending direct message: ' + e)
 
@@ -93,7 +93,7 @@ while 1:
         exit(255)
     except Exception as e:
         try:
-            twitter.direct_messages.new(user='mkzer0', text='Twitter Bot Error')
+            twitter.direct_messages.new(user=ADMIN_NAME, text='Twitter Bot Error')
         except Exception:
             pass
         print('Error loading current status: ' + e)
