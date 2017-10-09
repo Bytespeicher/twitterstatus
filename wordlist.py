caf9806674daa9ed04ee14dcdf573eaba162f0ed
Kuba Tyszko <kuba@lbl.pl>
2017-10-08 20:38:47 -0700

    polish translation

--- wordlist.py
+++ wordlist.py
@@ -94,7 +94,52 @@ translations = {
             'Gemein',
             'Schwach',
         ]
-    ]
+    ],
+    "pl_PL": [
+        [
+            'Spacja',
+            'Bytespeicher'
+        ],
+        [
+            'jest',
+        ],
+        [
+            'otwarte',
+            'dostępne',
+        ],
+        [
+            'zamknięte',
+            'juz zamknięte'
+        ],
+        [
+            'wspaniałe',
+            'imponujące',
+            'impresujące',
+            'inspirujące',
+            'ogromne',
+            'wspaniałe',
+            'majestatyczne',
+            'monumentalne',
+            'wspaniałe',
+            'wystawne',
+            'olsśiewajace',
+            'rozrzutne',
+            'piękne',
+            'zachwycające',
+            'kochane',
+        ],
+        [
+            'Słabe',
+            'niegodziwe',
+            'niechlubne',
+            'niegodziwe',
+            'okrutne',
+            'flagowe',
+            'pokorny',
+            'złośliwy',
+            'biedny',
+        ]
+    ],
 }
 
 
